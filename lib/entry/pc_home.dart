90612d007122e3f5a398ff340660358231db4aac
shAdowplusing <1290232854@qq.com>
2023-12-26 21:47:01 +0800

    boring to push something new

--- lib/entry/pc_home.dart
+++ lib/entry/pc_home.dart
@@ -11,28 +11,16 @@ class PCHomePage extends StatefulWidget {
 
 class _PCHomePageState extends State<PCHomePage> {
   String _selectedFont = 'NotoSerifSC';
+  int currentButtonIndex = -1;
+  bool showSettingArea = false;
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Stack(
         children: <Widget>[
-          // 左侧按钮区域
-          Positioned(
-            top: 0,
-            bottom: 0,
-            left: 0,
-            child: Container(
-              width: 100,  // 按钮区域宽度
-              child: Column(
-                children: List<Widget>.generate(6, (index) => ElevatedButton(
-                    onPressed: () {},
-                    child: Text('按钮 ${index + 1}')
-                )),
-              ),
-            ),
-          ),
           // 右侧滚动区域
           Positioned(
             top: 0,
@@ -54,6 +42,68 @@ class _PCHomePageState extends State<PCHomePage> {
                       child: Text('这里是底部的内容。'),
                     ),
                   ],
+                ),
+              ),
+            ),
+          ),
+          // 左侧按钮区域
+          Positioned(
+            top: 0,
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              width: 100,  // 按钮区域宽度
+              child: Column(
+                children: List<Widget>.generate(6, (index) => ElevatedButton(
+                    onPressed: () {
+                      debugPrint("click按钮 ${index + 1}");
+                      if (currentButtonIndex != index) {
+                        setState(() {
+                          currentButtonIndex = index;
+                          showSettingArea = true;
+                        });
+                      } else {
+                        setState(() {
+                          showSettingArea = !showSettingArea;
+                        });
+                      }
+                    },
+                    child: Text('按钮 ${index + 1}')
+                )),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 100,
+            child: Visibility(
+              visible: showSettingArea,
+              maintainSize: true,
+              maintainAnimation: true,
+              maintainState: true,
+              child: AnimatedOpacity(
+                opacity: showSettingArea ? 1.0 : 0.0,
+                duration: Duration(seconds: 1),
+                child: Container(
+                  width: size.width * 0.66,
+                  height: size.height * 0.66,
+                  color: Colors.red,
+                  padding: const EdgeInsets.all(16),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '这里是很长的文本...\n\n' * 100,  // 示例文本
+                          style: TextStyle(fontFamily: _selectedFont),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Text('这里是底部的内容。'),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
